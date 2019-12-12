d8f6bae244ade4f9fe36259e883e411cd32ba9de
Okery <13132515202@163.com>
2019-12-12 22:09:22 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# coding=utf-8
+"""
+@File    :   main.py    
+@Contact :   13132515202@163.com
+
+@Modify Time      @Author    @Version    @Description
+------------      -------    --------    -----------
+2019/12/12 21:58   LiuHe      1.0         客户端
+"""
+import wx
+
+app = wx.App()
+
+frm = wx.Frame(None, title="打印客户端")
+
+frm.Show()
+
+app.MainLoop()
